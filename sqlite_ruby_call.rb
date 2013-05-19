fa4a06d105acff327e79a8bd485d6f570406d403
pierre <iampierreandre@gmail.com>
2013-05-19 07:51:27 -0700

    updated score

--- sqlite_ruby_call.rb
+++ sqlite_ruby_call.rb
@@ -7,15 +7,18 @@ db = SQLite3::Database.new( "default.db" )
 #row = db.get_first_row( "SELECT name FROM `media` where id=19" )
 #rows = db.execute( "SELECT name FROM `media`" )
 
-confidence = db.execute("SELECT score FROM search_wtf")
 media = db.execute("SELECT name FROM media")
 search = db.execute("SELECT arg4 FROM search_criterion")
 
 
 confidence = db.execute("SELECT score FROM search_wtf")
+
+puts "Your search for: #{search[0]} Yielded: " 
+
 confidence.each do |num|
-    val=num[0]
-    puts val.to_f*100
+    val=num[0].to_f*100
+    val=val.ceil
+    puts "#{media[0]} : #{val}"
   end
   
 
